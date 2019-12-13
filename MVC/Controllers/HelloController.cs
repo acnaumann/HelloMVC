dffaff3e8848e8a24b6d4757fe6dcc054d1b4022
acnaumann <alysson12985@gmail.com>
2019-12-12 22:31:58 -0600

    Added language controller

--- MVC/Controllers/HelloController.cs
+++ MVC/Controllers/HelloController.cs
@@ -13,6 +13,14 @@ namespace MVC.Controllers
 
             string html = "<form method='post'>" +
                 "<input type='text' name='name'/>" +
+                "<select name='language' id='language-select'>" +
+                "<option value=''>Please choose an option</option>" +
+                "<option value='English'>English</option>" +
+                "<option value='German'>German</option>" +
+                "<option value='French'>French</option>" +
+                "<option value='Spanish'>Spanish</option>" +
+                "<option value='Czech'>Czech</option>" +
+                "</select>" +
                 "<input type='submit' value='Greet!'/>" +
                 "</form>";
 
@@ -22,25 +30,59 @@ namespace MVC.Controllers
         }
 
         // /Hello
+        //[Route("/Hello")]
+        //[HttpPost]
+        //public IActionResult Display(string name = "World")
+        //{
+        //    return Content(string.Format("<h1>Hello {0}<h1>", name), "text/html");
+        //}
+
+        ////Handle requests to /Hello/NAME (NAME = called URL segment - use as params that are passed )
+        //[Route("/Hello/{name}")]
+        //public IActionResult Index2(string name)
+        //{
+        //    return Content(string.Format("<h1>Hello {0}<h1>", name), "text/html");
+        //}
+
+        
+        //public IActionResult Goodbye()
+        //{
+        //    return Content("Goodbye");
+        //}
+
+
         [Route("/Hello")]
         [HttpPost]
-        public IActionResult Display(string name = "World")
+        public IActionResult CreateMessage(string language, string name)
         {
-            return Content(string.Format("<h1>Hello {0}<h1>", name), "text/html");
-        }
+            if (language == "English")
+            {
+                return Content(string.Format("Hello {0}", name));
+            }
+            else if (language == "Spanish")
+            {
+                return Content(string.Format("Hola {0}", name));
+            }
+            else if (language == "German")
+            {
+                return Content(string.Format("Hallo {0}", name + "! Wie geht's?"));
+            }
+            else if (language == "French")
+            {
+                return Content(string.Format("Bonjour {0}", name));
+            }
+            else if (language == "Czech")
+            {
+                return Content(string.Format("Ahoj {0}", name));
+            }
 
-        //Handle requests to /Hello/NAME (NAME = called URL segment - use as params that are passed )
-        [Route("/Hello/{name}")]
-        public IActionResult Index2(string name)
-        {
-            return Content(string.Format("<h1>Hello {0}<h1>", name), "text/html");
-        }
+            else
+            {
+                return null;
+            }
 
-        public Ia
-        
-        public IActionResult Goodbye()
-        {
-            return Content("Goodbye");
+
+            
         }
     }
 }
